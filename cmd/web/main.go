dcd81dcbf26f4f28bf8cc3e6c3157fcb1f4cc90b
namatoj <namatoj@gmail.com>
2024-02-19 20:40:16 +0000

    feat: add login route

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -15,6 +17,19 @@ func main() {
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.POST("/login/", func(c echo.Context) error {
+			// data := apis.RequestInfo(c).Data
+			email := c.FormValue("email")
+			password := c.FormValue("password")
+			print(email, password)
+
+			return c.HTML(http.StatusOK, "/login")
+		})
+
 		return nil
 	})
 
