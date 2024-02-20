844d90ad8057f15822715c214686c3a85c94d45f
namatoj <namatoj@gmail.com>
2024-02-20 19:24:39 +0000

    feat: add middleware to check the validity of the authentication token in the cookie

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,7 +10,48 @@ import (
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/tokens"
+	"github.com/pocketbase/pocketbase/tools/security"
+	"github.com/spf13/cast"
 )
+
+func loadAuthContextFromCookie(app core.App) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			tokenCookie, err := c.Request().Cookie("token")
+			if err != nil || tokenCookie.Value == "" {
+				return next(c) // no token cookie
+			}
+
+			token := tokenCookie.Value
+
+			claims, _ := security.ParseUnverifiedJWT(token)
+			tokenType := cast.ToString(claims["type"])
+
+			switch tokenType {
+			case tokens.TypeAdmin:
+				admin, err := app.Dao().FindAdminByToken(
+					token,
+					app.Settings().AdminAuthToken.Secret,
+				)
+				if err == nil && admin != nil {
+					// "authenticate" the admin
+					c.Set(apis.ContextAdminKey, admin)
+				}
+			case tokens.TypeAuthRecord:
+				record, err := app.Dao().FindAuthRecordByToken(
+					token,
+					app.Settings().RecordAuthToken.Secret,
+				)
+				if err == nil && record != nil {
+					// "authenticate" the app user
+					c.Set(apis.ContextAuthRecordKey, record)
+				}
+			}
+
+			return next(c)
+		}
+	}
+}
 
 func main() {
 	app := pocketbase.New()
@@ -18,6 +59,11 @@ func main() {
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.Use(loadAuthContextFromCookie(app))
 		return nil
 	})
 
@@ -49,7 +95,27 @@ func main() {
 				SameSite: http.SameSiteLaxMode,
 			})
 
-			return c.HTML(http.StatusOK, "/login")
+			return c.HTML(http.StatusOK, "/login/")
+		})
+
+		return nil
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/", func(c echo.Context) error {
+			info := apis.RequestInfo(c)
+			admin := info.Admin       // nil if not authenticated as admin
+			record := info.AuthRecord // nil if not authenticated as regular auth record
+
+			isGuest := admin == nil && record == nil
+
+			if isGuest {
+				return c.HTML(http.StatusOK, "not logged in")
+
+			}
+
+			return c.HTML(http.StatusOK, "logged in")
+
 		})
 
 		return nil
