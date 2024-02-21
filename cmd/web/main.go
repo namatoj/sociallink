ae79e758685609dbbe40aa12e37fd08ea453ba06
namatoj <namatoj@gmail.com>
2024-02-21 20:59:07 +0000

    refactor: move route definitions and app creation from main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/namatoj/sociallink/pkg/web"
-	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
-	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-	app := pocketbase.New()
-
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.Use(web.LoadAuthContextFromCookie(app))
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
-		e.Router.POST("/login/", web.LoginHandler(app))
-		e.Router.GET("/logout/", web.LogoutHandler)
-		e.Router.GET("/", web.RootHandler)
-		return nil
-	})
+	app := web.App()
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
--- pkg/web/start.go
+++ pkg/web/start.go
@@ -2,10 +2,28 @@ package web
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
+
+func App() *pocketbase.PocketBase {
+	app := pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.Use(LoadAuthContextFromCookie(app))
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		e.Router.POST("/login/", LoginHandler(app))
+		e.Router.GET("/logout/", LogoutHandler)
+		e.Router.GET("/", RootHandler)
+		return nil
+	})
+
+	return app
+}
 
 func RootHandler(c echo.Context) error {
 	info := apis.RequestInfo(c)
